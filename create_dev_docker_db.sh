f5d8bc735bce13d3d768dc093b2ccfda32bd8740
muzyk0 <ru9art@gmail.com>
2022-02-09 21:40:32 +0300

    fix docker-compose

--- create_dev_docker_db.sh
+++ create_dev_docker_db.sh
@@ -6,8 +6,7 @@
 docker-compose -p todolist-graphql-server build && \
 docker-compose -p todolist-graphql-server up -d postgres && \
 sleep 5 # надо дать время базе данных на загрузку и подготовку к работе
-echo postgrespassword | docker-compose -p todolist-graphql-server run --rm --no-deps postgres psql -h postgres -U postgres -c "CREATE DATABASE test;"
-# echo postgrespassword | docker-compose -p todolist-graphql-server run --rm --no-deps postgres psql -h postgres -U postgres -c "CREATE DATABASE testing;"
+echo DBPassword | docker-compose -p todolist-graphql-server run --rm --no-deps postgres psql -h postgres -U postgres -c "CREATE DATABASE graphql_todolist;"
 echo 'rm -rf dist/* && yarn tsc && yarn typeorm migration:run' | docker-compose -p todolist-graphql-server run --rm server sh 
 docker-compose -p todolist-graphql-server down
 # docker-compose -p todolist-graphql-server up
